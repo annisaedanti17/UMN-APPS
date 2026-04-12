66346356827f881ae3670266da8ec97a782a0df2
annisaedanti17 <annisaekadanti@gmail.com>
2026-04-13 04:20:23 +0700

    perubahan berita dan Pendaftaran RLP

--- app/src/main/java/com/example/umnapps/PendaftaranRLP.java
+++ app/src/main/java/com/example/umnapps/PendaftaranRLP.java
@@ -13,48 +13,30 @@ public class PendaftaranRLP extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pendaftaran_rlp);
 
-        // Inisialisasi Tombol Navigasi Bawah
-        LinearLayout navHome = findViewById(R.id.navHome);
-        LinearLayout navProfil = findViewById(R.id.navProfil);
-        LinearLayout navFakultas = findViewById(R.id.navFakultas);
-        LinearLayout navFasilitas = findViewById(R.id.navFasilitas);
         // Inisialisasi Tombol Back
         ImageView btnBack = findViewById(R.id.btnBack);
+        ImageView btnRplS1 = findViewById(R.id.  btnRplS1);
+        ImageView btnRplS2 = findViewById(R.id.  btnRplS2);
 
-
-
-        // Navigasi ke Home
-        if (navHome != null) {
-            navHome.setOnClickListener(v -> {
-                startActivity(new Intent(PendaftaranRLP.this, HomeActivity.class));
-            });
-        }
-
-        // Navigasi ke Profil
-        if (navProfil != null) {
-            navProfil.setOnClickListener(v -> {
-                startActivity(new Intent(PendaftaranRLP.this, ProfilActivity.class));
-            });
-        }
-
-        // Navigasi ke Fakultas
-        if (navFakultas != null) {
-            navFakultas.setOnClickListener(v -> {
-                startActivity(new Intent(PendaftaranRLP.this, FakultasActivity.class));
-            });
-        }
-
-        // Navigasi ke Fasilitas
-        if (navFasilitas != null) {
-            navFasilitas.setOnClickListener(v -> {
-                startActivity(new Intent(PendaftaranRLP.this, FasilitasActivity.class));
-            });
-        }
 
         // Logika Klik Tombol Back
         if (btnBack != null) {
             btnBack.setOnClickListener(v -> {
                 finish(); // Ini akan menutup halaman dan kembali ke halaman sebelumnya
+            });
+        }
+
+        if (btnRplS1 != null) {
+            btnRplS1.setOnClickListener(v -> {
+                Intent intent = new Intent(PendaftaranRLP.this, JalurRPL1.class);
+                startActivity(intent);
+            });
+        }
+
+        if (btnRplS2 != null) {
+            btnRplS2.setOnClickListener(v -> {
+                Intent intent = new Intent(PendaftaranRLP.this, JalurRPL2.class);
+                startActivity(intent);
             });
         }
     }
